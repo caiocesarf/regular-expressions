c97a6240af470ac17c751d47ea5e20748aa02179
caio fernandes <caio.fernandes@prodigasistemas.com.br>
2024-05-12 10:33:34 -0300

    improving regex

--- family.py
+++ family.py
@@ -6,9 +6,12 @@ def check_family(family, regex):
 
 # a) Casais heterossexuais mais velhos que os filhos com pelo menos duas filhas mulheres,
 # ou pelo menos um filho homem, ou ainda pelo menos dois filhos homens e uma filha mulher.
-regex_a = r"(HM|MH)(mm|hh)(hh|m)"
+regex_a = r"(HM|MH)(mm|hh)(hh|m)*"
 print("a)", check_family("HMhhm", regex_a))  # True
 print("a)", check_family("HMm", regex_a))    # False
+print("a)", check_family("HMmm", regex_a))    # true
+print("a)", check_family("HMhh", regex_a))  # True
+print("a)", check_family("HMhhm", regex_a))  # True
 
 # b) Casais heterossexuais mais velhos que os filhos e com uma quantidade ímpar de filhas
 # mulheres.
@@ -18,22 +21,23 @@ print("b)", check_family("HMmmmm", regex_b)) # False
 
 # c) Casais heterossexuais mais velhos que os filhos, com a filha mais velha mulher e o filho
 # mais novo homem.
-regex_c = r"(HM|MH)(mh)(m/h)*"
-print("c)", check_family("HMmh", regex_c))    # True
+regex_c = r"(HM|MH)(m+((m|h)*(h|m)*)h+)"
+print("c)", check_family("HMmmhmmh", regex_c))    # True
 print("c)", check_family("HMhm", regex_c))    # False
 
 # d) Casais homossexuais mais velhos que os filhos, com pelo menos seis filhos, em que os
 # dois primeiros filhos formam um casal e os últimos também.
-regex_d = r"HH(hh|mm){2,}(hh|mm)"
-print("d)", check_family("HHhhhhhh", regex_d)) # True
+regex_d = r"(HH|MM)((hm|mh))((m|h)+(h|m)+)((hm|mh))"
+#regex_d = r"HH(hh|mm){2,}(hh|mm)"
+print("d)", check_family("HHhmmmmh", regex_d)) # True
 print("d)", check_family("HHhhmmhh", regex_d))  # True
 print("d)", check_family("HHhhmm", regex_d))    # False
 
 # e) Casais homossexuais mais velhos que os filhos, em que o sexo dos filhos é alternado
 # conforme a ordem de nascimento.
-regex_e = r"HH(hm)+|(MM(mh)+)"
-print("e)", check_family("HHhmhmhm", regex_e)) # True
-print("e)", check_family("HHhmhmh", regex_e))  # False
+regex_e = r"(HH|MM)((hm)|(mh))(hm)*(mh)*"
+print("e)", check_family("HHmhhm", regex_e)) # True
+print("e)", check_family("HHhmhmhm", regex_e))  # False
 
 # f) Casais homossexuais mais velhos que os filhos, com qualquer quantidade de filhos
 # homens e mulheres, mas que não tiveram dois filhos homens consecutivos.
