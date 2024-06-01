0473e69a2a179eaf8996270cc087910252a7fd95
caio fernandes <caio.fernandes@prodigasistemas.com.br>
2024-06-01 18:06:33 -0300

    segunda questao

---
+++ automatous/findWord.py
@@ -0,0 +1,41 @@
+def build_dfa(pattern):
+    M = len(pattern)
+    dfa = [{} for _ in range(M)]
+    dfa[0][pattern[0]] = 1
+
+    X = 0
+    for j in range(1, M):
+        for c in range(256):
+            dfa[j][chr(c)] = dfa[X].get(chr(c), 0)
+        dfa[j][pattern[j]] = j + 1
+        X = dfa[X].get(pattern[j], 0)
+
+    return dfa
+
+
+def search(text, pattern):
+    dfa = build_dfa(pattern)
+    M = len(pattern)
+    N = len(text)
+    j = 0
+    positions = []
+
+    for i in range(N):
+        j = dfa[j].get(text[i], 0)
+        if j == M:
+            positions.append(i - M + 1)
+            j = 0
+
+    return positions
+
+
+T = (
+    "O computador é uma máquina capaz de variados tipos de tratamento automático de informações ou processamento de dados. "
+    "Entende-se por computador um sistema físico que realiza algum tipo de computação. Assumiu-se que os computadores pessoais "
+    "e laptops são ícones da era da informação. O primeiro computador eletromecânico foi construído por Konrad Zuse (1910–1995). "
+    "Atualmente, um microcomputador é também chamado computador pessoal ou ainda computador doméstico.")
+
+pattern = "computador"
+positions = search(T, pattern)
+
+print(f"A palavra '{pattern}' ocorre nas posições: {positions}")
